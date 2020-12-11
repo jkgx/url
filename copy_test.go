8484728a1635769b2cbffe91fc39d4856b7a1f6e
Julian <me@julian.pro>
2020-12-11 17:23:13 +0100

    first commit

---
+++ copy_test.go
@@ -0,0 +1,22 @@
+package url
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCopyWithQuery(t *testing.T) {
+	a, _ := url.Parse("https://google.com/foo?bar=baz")
+	b := CopyWithQuery(a, url.Values{"foo": {"bar"}})
+	assert.NotEqual(t, a.String(), b.String())
+	assert.Equal(t, "bar", b.Query().Get("foo"))
+}
+
+func TestCopy(t *testing.T) {
+	a, _ := url.Parse("https://google.com/foo?bar=baz")
+	b := Copy(a)
+	b.Path = "bar"
+	assert.NotEqual(t, a.String(), b.String())
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jkgx/url
+
+go 1.15
+
+require (
+	github.com/jkgx/cmd v0.0.0-20201211154252-23b4f865b393
+	github.com/jkgx/logrus v0.0.0-20201211153830-04212ac7fc72
+	github.com/stretchr/testify v1.6.1
+)
